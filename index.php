a6204bb290197d94056e55c7eb66bb7bd0492758
Clayton Phillipp <phillipp@Claytons-MacBook-Pro.local>
2012-07-11 20:25:10 -0600

    updated index page

--- index.php
+++ index.php
@@ -6,10 +6,10 @@ if(isset($_SESSION['message'])) {
   $_SESSION['message'] = '';
 }
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html lang="en">
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <meta charset="utf-8" />
   <meta name="description" content="Getting a website in Idaho Falls is easy with Claymore Design. We are an Idaho Falls Web Design company building websites that are optimized &amp; easily found all over the web."/>
   <meta name="keywords" content="idaho falls web design, web design idaho falls, idaho web design, web design idaho, idaho falls seo, seo idaho falls, idaho seo, seo idaho, idaho falls marketing, marketing idaho falls, idaho marketing, marketing idaho, idaho falls website design, website design idaho falls, idaho website design, website design idaho"/>
   <title>Idaho Falls Web Design | Affordable websites for Idaho Falls businesses | Claymore Design</title>
