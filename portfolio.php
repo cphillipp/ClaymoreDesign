afbb5ae8c77b2e02db446119efe719d8050fd47a
Clayton Phillipp <phillipp@Claytons-MacBook-Pro.local>
2012-07-11 23:44:58 -0600

    updated google analytics

--- portfolio.php
+++ portfolio.php
@@ -32,6 +32,21 @@ if(isset($_SESSION['message'])) {
       .wrapper {display:table;height:100%}
     </style>
   <![endif]-->
+  <script type="text/javascript">
+
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-33238695-1']);
+    _gaq.push(['_setDomainName', 'claymoredesigns.com']);
+    _gaq.push(['_setAllowLinker', true]);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+
+  </script>
 </head>
 <body>
   <div class="wrapper">
